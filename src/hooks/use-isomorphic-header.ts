519608fa32d101ba742bc6dc29fa2849aaba7231
George <71434803+george-swift@users.noreply.github.com>
2025-08-06 18:26:32 +0100

    feat: add useIsomorphicHeader hook for main header styling in landing page

---
+++ src/hooks/use-isomorphic-header.ts
@@ -0,0 +1,82 @@
+import { useEffect, useRef } from 'react'
+
+import { clamp } from '@/lib/utils'
+
+export const useIsomorphicHeader = () => {
+  const headerRef = useRef<React.ComponentRef<'div'>>(null)
+  const isInitial = useRef(true)
+
+  useEffect(() => {
+    const downDelay = headerRef.current?.offsetTop ?? 0
+    const upDelay = 64
+
+    function setProperty(property: string, value: string) {
+      document.documentElement.style.setProperty(property, value)
+    }
+
+    function removeProperty(property: string) {
+      document.documentElement.style.removeProperty(property)
+    }
+
+    function updateHeaderStyles() {
+      if (!headerRef.current) {
+        return
+      }
+
+      const { height, top } = headerRef.current.getBoundingClientRect()
+      const scrollY = clamp(
+        window.scrollY,
+        0,
+        document.body.scrollHeight - window.innerHeight
+      )
+
+      if (isInitial.current) {
+        setProperty('--header-position', 'sticky')
+      }
+
+      setProperty('--content-offset', `${downDelay}px`)
+
+      if (isInitial.current || scrollY < downDelay) {
+        setProperty('--header-height', `${downDelay + height}px`)
+        setProperty('--header-mb', `${-downDelay}px`)
+      } else if (top + height < -upDelay) {
+        const offset = Math.max(height, scrollY - upDelay)
+        setProperty('--header-height', `${offset}px`)
+        setProperty('--header-mb', `${height - offset}px`)
+      } else if (top === 0) {
+        setProperty('--header-height', `${scrollY + height}px`)
+        setProperty('--header-mb', `${-scrollY}px`)
+      }
+
+      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
+        setProperty('--header-inner-position', 'fixed')
+        removeProperty('--header-top')
+      } else {
+        removeProperty('--header-inner-position')
+        setProperty('--header-top', '0px')
+      }
+
+      if (scrollY === 0) {
+        document.documentElement.classList.add('at-top-header')
+      } else {
+        document.documentElement.classList.remove('at-top-header')
+      }
+    }
+
+    function updateStyles() {
+      updateHeaderStyles()
+      isInitial.current = false
+    }
+
+    updateStyles()
+    window.addEventListener('scroll', updateStyles, { passive: true })
+    window.addEventListener('resize', updateStyles)
+
+    return () => {
+      window.removeEventListener('scroll', updateStyles)
+      window.removeEventListener('resize', updateStyles)
+    }
+  }, [])
+
+  return { headerRef }
+}
