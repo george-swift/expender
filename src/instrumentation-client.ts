4643fca04c1159cb724d2de890a0e0d7bb9c1512
George <71434803+george-swift@users.noreply.github.com>
2025-08-07 00:11:40 +0100

    feat: set up instrumentation, register Sentry and add global error component

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,31 @@
+// This file configures the initialization of Sentry on the client.
+// The config added here will be used whenever users load a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+import { env } from '@/env'
+
+Sentry.init({
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Sentry integration for Session Replay
+  integrations: [Sentry.replayIntegration()],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 0.5,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Define how likely Replay events are sampled. This sets the sample rate to be 10%.
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+})
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
