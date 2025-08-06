cf2cb81599184e09ba6cef0646b39190f145c36d
George <71434803+george-swift@users.noreply.github.com>
2025-08-07 00:10:27 +0100

    feat: integrate Sentry edge and server config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import { withSentryConfig } from '@sentry/nextjs'
 
 import { env } from '@/env'
 
@@ -17,4 +18,16 @@ const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx']
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  org: env.NEXT_PUBLIC_SENTRY_ORG,
+  project: env.NEXT_PUBLIC_SENTRY_PROJECT,
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // Upload a larger set of source maps for prettier stack traces
+  widenClientFileUpload: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true
+})
