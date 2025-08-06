0edbe57df3e51f3081e86fcf06f2f7d90b91cd85
George <71434803+george-swift@users.noreply.github.com>
2025-08-06 15:52:25 +0100

    feat: integrate ThemeProvider

---
+++ src/app/providers/next-themes.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export default function ThemeProvider({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
+}
