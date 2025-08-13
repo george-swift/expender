29d402bb997843262fc00d8f4f1448c30aa39a16
George <71434803+george-swift@users.noreply.github.com>
2025-08-13 01:32:30 +0100

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist_Mono } from 'next/font/google'
 import localFont from 'next/font/local'
+import { Analytics } from '@vercel/analytics/next'
 
 import { Toaster } from '@/components/ui/sonner'
 import { Providers } from '@/app/providers'
@@ -134,6 +135,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster richColors />
+          <Analytics />
         </Providers>
       </body>
     </html>
