79ebd024c98bf3392f81b1bec550c7c694e9ff1f
George <71434803+george-swift@users.noreply.github.com>
2025-09-29 23:30:35 +0100

    feat(redesign): update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,33 @@
 import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
-import { Container } from '@/components/container'
+import { SiteHeader } from '@/components/marketing/site-header'
 import { buttonVariants } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <Container className="flex flex-col items-center gap-4 h-full pt-16 sm:pt-32">
-      <p className="text-base font-semibold">404</p>
-      <p className="text-4xl font-bold tracking-tight">Page not found</p>
-      <p className="text-base text-muted-foreground">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <Link href="/" className={cn(buttonVariants({ variant: 'outline' }))}>
-        Go back home
-      </Link>
-    </Container>
+    <div className="flex min-h-screen flex-col">
+      <SiteHeader />
+      <div className="flex-1 flex flex-col items-center justify-center gap-6 max-w-7xl mx-auto px-4 lg:px-6 lg:pt-40">
+        <div className="space-y-5 w-75 text-center text-balance">
+          <div className="text-[32px]/[38px] font-medium">
+            Oops! This page was not found.
+          </div>
+          <div className="text-sm text-muted-foreground">
+            You may have mistyped the address or the page may have moved.
+          </div>
+        </div>
+
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: 'ghost' }),
+            'h-11 bg-brand font-normal'
+          )}
+        >
+          Go back home
+        </Link>
+      </div>
+    </div>
   )
 }
