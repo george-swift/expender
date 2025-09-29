c75caaf916f135ed49e3a9f38151cb5b64a54c18
George <71434803+george-swift@users.noreply.github.com>
2025-09-29 23:35:13 +0100

    refactor: add placeholder for support page

--- src/app/(marketing)/support/page.tsx
+++ src/app/(marketing)/support/page.tsx
@@ -1,4 +1,4 @@
-export function SupportPage() {
+export default function SupportPage() {
   return (
     <div className="px-4 lg:px-6 max-w-5xl mx-auto space-y-10">
       <h1 className="text-4xl font-medium">Support</h1>
